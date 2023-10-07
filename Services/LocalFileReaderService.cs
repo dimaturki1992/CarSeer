077e36e9302f54a80655b6c20ed69fc386f1aebc
Dima Turki <dima.turki@hotmail.com>
2023-10-07 07:21:40 +0300

    file reader service

---
+++ Services/LocalFileReaderService.cs
@@ -0,0 +1,15 @@
+﻿using CarSeer.Interfaces;
+using System.Text;
+
+namespace CarSeer.Services
+{
+    public class LocalFileReaderService: ILocalFileReaderService
+    {
+        public StreamReader LoadFile(string relativeFilePath)
+        {
+            string filePath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, relativeFilePath);
+            FileStream stream = new FileStream(filePath, FileMode.Open);
+            return new StreamReader(stream, Encoding.UTF8);
+        }
+    }
+}
